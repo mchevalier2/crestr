e4f15a0e89d03466c9e905f90115ce5ccbaa83ad
Manu Chevalier <chevalier.manuel@gmail.com>
2020-06-03 16:35:14 +0200

    dbRequest

---
+++ R/dbRequest.R
@@ -0,0 +1,21 @@
+#' Connect to the gbif4crest database
+#'
+#' Connect to the gbif4crest_02 database by accessing the server on Amazon.
+#'
+#' @param request A SQL request to be executed.
+#' @return The result of the request.
+#' @export
+#' @examples
+#' dbRequest("SELECT count(*) FROM taxa")
+#' southAfricaTaxa <- dbRequest("SELECT taxa.* FROM taxa, distrib_qdgc, geo_qdgc
+#'     WHERE taxa.taxonid=distrib_qdgc.taxonid
+#'     AND   distrib.qdgc.latitude=geo_qdgc.latitude
+#'     AND   distrib_qdgc.longitude=geo_qdgc.longitude
+#'     AND geo_qdgc.countryname='South Africa'")
+
+dbRequest <- function(request) {
+    db <- connect_online()
+    res <- DBI::dbGetQuery(db, request)
+    close_db_connection(db)
+    res
+}
