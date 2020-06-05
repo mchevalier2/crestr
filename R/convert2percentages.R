6ac4417e5b6fe4144f2e9dff909b1befaf8f59a5
Manu Chevalier <chevalier.manuel@gmail.com>
2020-06-05 09:18:58 +0200

    convert2presenceAbsence

--- R/convert2percentages.R
+++ R/convert2percentages.R
@@ -21,3 +21,29 @@ convert2percentages <- function(df, col2convert = 2:ncol(df) ) {
     rownames(df2) <- rownames(df)
     df2
 }
+
+
+#' Convert data into presence/absence data.
+#'
+#' Convert data into presence/absence data.
+#'
+#' @param df The dataframe containing the data to convert.
+#' @param threshold The threshold that defines presence (presence if >= threshold)
+#' @param col2convert A vector of the columns to convert. Default is all the
+#'    columns but the first, which contains an age, a depth or a sampleID.
+#' @return A vector of unique taxonIDs.
+#' @export
+#' @examples
+#' df = data.frame(matrix(1:25, ncol = 5))
+#' colnames(df) = paste( rep("col", 5), 1:5, sep = '')
+#' convert2presenceAbsence(df, threshold = 15)
+#' convert2presenceAbsence(df, col2convert = 3:5)
+
+convert2presenceAbsence <- function(df, threshold = 2, col2convert = 2:ncol(df) ) {
+    df2 <- cbind( df[, -col2convert],
+                  ifelse(df[, col2convert] >= threshold, 1, 0)
+                 )
+    colnames(df2) <- colnames(df)
+    rownames(df2) <- rownames(df)
+    df2
+}
