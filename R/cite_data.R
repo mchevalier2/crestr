429e05db3ed5d5b601cde334558d30cf2f194162
mchevali2 <chevalier.manuel@gmail.com>
2021-02-24 17:55:27 +0100

    update cite_data()

--- R/cite_data.R
+++ R/cite_data.R
@@ -7,7 +7,7 @@
 #'        \code{\link{crest.get_modern_data}}, \code{\link{crest.calibrate}},
 #'        \code{\link{crest.reconstruct}} or \code{\link{loo}} functions.
 #' @export
-
+#'
 cite_crest <- function(x, dbname = "gbif4crest_02", verbose=TRUE) {
     tocite <- list()
     tocite$distrib <- cite_distrib_data(x, dbname, verbose=FALSE)
@@ -29,6 +29,7 @@ cite_crest <- function(x, dbname = "gbif4crest_02", verbose=TRUE) {
     invisible(tocite)
 }
 
+
 #' Returns the references associated with the GBIF data used to fit the \code{pdfs}.
 #'
 #' Returns the references associated with the GBIF data used to fit the \code{pdfs}.
@@ -38,7 +39,7 @@ cite_crest <- function(x, dbname = "gbif4crest_02", verbose=TRUE) {
 #'        \code{\link{crest.get_modern_data}}, \code{\link{crest.calibrate}},
 #'        \code{\link{crest.reconstruct}} or \code{\link{loo}} functions.
 #' @export
-
+#'
 cite_distrib_data <- function(x, dbname = "gbif4crest_02", verbose=TRUE) {
     if (x$parameters$taxaType == 1) {
         list_of_classes <- c()
@@ -94,15 +95,15 @@ cite_distrib_data <- function(x, dbname = "gbif4crest_02", verbose=TRUE) {
 #'        \code{\link{crest.get_modern_data}}, \code{\link{crest.calibrate}},
 #'        \code{\link{crest.reconstruct}} or \code{\link{loo}} functions.
 #' @export
-
+#'
 cite_climate_data <- function(x, dbname = "gbif4crest_02", verbose=TRUE) {
     if (x$parameters$taxaType == 0) {
         if (verbose)  cat('You have used the example dataset. No climate data citations are required.\n')
         tocite <- NULL
     } else {
-        tocite <- "Fick, S.E. and Hijmans, R.J., 2017, WorldClim 2: new 1-km spatial resolution climate surfaces for global land areas. International Journal of Climatology, 37, pp. 4302–4315."
+        tocite <- "Fick, S.E. and Hijmans, R.J., 2017, WorldClim 2: new 1-km spatial resolution climate surfaces for global land areas. International Journal of Climatology, 37, pp. 4302-4315."
         if ('ai' %in% x$parameters$climate) {
-            tocite <- c(tocite, "Zomer, R.J., Trabucco, A., Bossio, D.A. and Verchot, L. V., 2008, Climate change mitigation: A spatial analysis of global land suitability for clean development mechanism afforestation and reforestation. Agriculture, Ecosystems \u0026 Environment, 126, pp. 67–80.")
+            tocite <- c(tocite, "Zomer, R.J., Trabucco, A., Bossio, D.A. and Verchot, L. V., 2008, Climate change mitigation: A spatial analysis of global land suitability for clean development mechanism afforestation and reforestation. Agriculture, Ecosystems \u0026 Environment, 126, pp. 67-80.")
         }
         if (verbose) {
             cat('Please cite the following dataset(s):\n')
@@ -125,11 +126,11 @@ cite_climate_data <- function(x, dbname = "gbif4crest_02", verbose=TRUE) {
 #'        \code{\link{crest.get_modern_data}}, \code{\link{crest.calibrate}},
 #'        \code{\link{crest.reconstruct}} or \code{\link{loo}} functions.
 #' @export
-
+#'
 cite_method <- function(x, dbname = "gbif4crest_02", verbose=TRUE) {
-    tocite <- 'Chevalier, M., Cheddadi, R. and Chase, B.M., 2014, CREST (Climate REconstruction SofTware): a probability density function (PDF)-based quantitative climate reconstruction method. Climate of the Past, 10, pp. 2081–2098. doi: 10.5194/cp-10-2081-2014'
+    tocite <- 'Chevalier, M., Cheddadi, R. and Chase, B.M., 2014, CREST (Climate REconstruction SofTware): a probability density function (PDF)-based quantitative climate reconstruction method. Climate of the Past, 10, pp. 2081-2098. doi: 10.5194/cp-10-2081-2014'
     if (x$parameters$taxaType > 0) {
-        tocite <- c(tocite, 'Chevalier, M., 2019, Enabling possibilities to quantify past climate from fossil assemblages at a global scale. Global and Planetary Change, 175, pp. 27–35. doi: 10.1016/j.gloplacha.2019.01.016')
+        tocite <- c(tocite, 'Chevalier, M., 2019, Enabling possibilities to quantify past climate from fossil assemblages at a global scale. Global and Planetary Change, 175, pp. 27-35. doi: 10.1016/j.gloplacha.2019.01.016')
     }
     if (verbose) {
         cat('Please cite the following papers(s):\n')
